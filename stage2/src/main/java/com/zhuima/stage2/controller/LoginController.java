28f63005c52a6ddf7f96890eac75ac9ff0df9f9d
zhuima <zhuima314@gmail.com>
2022-09-02 15:54:13 +0800

    complate stage2-chapter3

--- stage2/src/main/java/com/zhuima/stage2/controller/LoginController.java
+++ stage2/src/main/java/com/zhuima/stage2/controller/LoginController.java
@@ -3,11 +3,15 @@ package com.zhuima.stage2.controller;
 import com.zhuima.stage2.domain.User;
 import com.zhuima.stage2.form.UserForm;
 import com.zhuima.stage2.service.UserService;
-import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
+import java.util.List;
 
 @Controller
 public class LoginController {
@@ -28,7 +32,31 @@ public class LoginController {
 
 
     @PostMapping("/register")
-    public String registerPost(UserForm userForm) {
+    public String registerPost(@Valid UserForm userForm, BindingResult result) {
+        boolean boo = true;
+
+        if (!userForm.confirmPassword()) {
+            result.rejectValue( "confirmPassword",  "confirmError", "两次密码不一致");
+            boo = false;
+        }
+
+
+
+        if (result.hasErrors()) {
+            List<FieldError> fieldErrors  = result.getFieldErrors();
+
+            for (FieldError fieldError : fieldErrors) {
+                System.out.println(fieldError.getField() + " " + fieldError.getDefaultMessage() + " " + fieldError.getCode());
+            }
+            boo = false;
+        }
+
+
+
+        if (!boo) {
+            return "register";
+        }
+
         User user = userForm.convertToUser();
         userService.saveUser(user);
         return "redirect:/login";
--- stage2/src/main/java/com/zhuima/stage2/form/UserForm.java
+++ stage2/src/main/java/com/zhuima/stage2/form/UserForm.java
@@ -2,15 +2,40 @@ package com.zhuima.stage2.form;
 
 import com.zhuima.stage2.domain.User;
 import lombok.Data;
+import org.hibernate.validator.constraints.Length;
 import org.springframework.beans.BeanUtils;
+
+import javax.validation.constraints.*;
 
 @Data
 public class UserForm {
+
+    private static final String REGEXP_PHONE = "^1[3456789]\\d{9}$";
+
+
+    @NotEmpty(message = "用户名称不能为空")
     private String username;
+    @Email(message = "邮箱地址不合法")
+    @NotEmpty(message = "邮箱不能为空")
     private String email;
+    @Size(min = 6, max = 16, message = "密码长度必须在{min}-{max}之间")
     private String password;
+    @Size(min = 11, max = 11, message = "手机号不合法")
+    @NotEmpty(message = "手机号不能为空")
+    @Pattern(message = "手机号不合法", regexp = REGEXP_PHONE )
     private String phone;
+    @Size(min = 6, max = 16, message = "密码长度必须在{min}-{max}之间")
     private String confirmPassword;
+
+
+
+    public boolean confirmPassword() {
+
+        if (this.getPassword().equals(this.getConfirmPassword())) {
+            return true;
+        }
+        return false;
+    }
 
 
     public User convertToUser( ) {
